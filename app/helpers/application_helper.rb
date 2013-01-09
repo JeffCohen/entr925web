d068e1da7e5e14ee3b86002cb725d05b0baf259c
Jeff Cohen <jeff@codeacademy.org>
2013-01-09 10:33:06 -0600

    Revamp a bit

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,12 @@ module ApplicationHelper
     "http://github.com/cspp52553/homework".html_safe
   end
 
+  def piazza_url
+    "https://piazza.com/uchicago/winter2013/cspp52553/home".html_safe
+  end
+
+  def syllabus_path
+    "/syllabus.pdf".html_safe
+  end
+
 end
