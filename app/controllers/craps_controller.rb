b027cce8dc445b80c8d3f4d829ced37c68d487b8
Jeff Cohen <jeff@codeacademy.org>
2013-01-21 22:46:20 -0600

    fix dice game

--- app/controllers/craps_controller.rb
+++ app/controllers/craps_controller.rb
@@ -6,11 +6,13 @@ class CrapsController < ApplicationController
 
   def roll
     @point = params[:point]
-    logger.info "Here"
     @dice = [rand(1..6), rand(1..6)]
     @total = (@dice.first + @dice.last)
-    @win = @point ? (@point == @total) : ([7, 11].include?(@total))
+    @win = @point ? (@point.to_i == @total) : ([7, 11].include?(@total))
     @lose = @point ? ([7, 11].include?(@total)) : ([2, 3, 12].include?(@total))
+    logger.info "Point: #{@point}"
+    logger.info "Total: #{@total}"
+    logger.info "Win: #{@win}"
 
     render 'show'
   end
